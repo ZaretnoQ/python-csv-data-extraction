05fa877bff116ac52a2e543250c95eed51577dd7
ZaretnoQ <christianseniel2@gmail.com>
2025-06-06 23:44:02 +0800

    Initial Commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import csv
+
+class Book:
+    def __init__(self, book, sale):
+        self.book = book
+        self.sale = sale
+
+with open('Bestseller - Sheet1.csv', 'r', encoding='utf8') as file:
+    csv_reader = csv.reader(file)
+
+    book = Book('name', 0.0)
+    next(csv_reader)
+
+    for i in csv_reader:
+        sale = float(i[4])
+
+        if sale >= book.sale:
+            book = Book(i[0], sale)
+
+    #for i in range(len(book_list)):
+    print(f"{book.book} - {book.sale}")
